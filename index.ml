f5b23291cbb65c69aa7173e8eade544c6413ea05
Xavier Deschuyteneer <xavier.deschuyteneer@gmail.com>
2014-07-21 22:12:21 +0200

    add stars mesh

--- index.ml
+++ index.ml
@@ -18,23 +18,6 @@ let get_input id =
 let get_button id =
   by_id_coerce id Dom_html.CoerceTo.button;;
 
-let set_button_click_event button f =
-  button##onclick <-
-    Dom_html.handler
-      (fun ev ->
-       begin
-         f ();
-         Js._true;
-       end);;
-
-let create_canvas w h =
-  let d = Dom_html.window##document in
-  let c = Dom_html.createCanvas d in
-  c##width <- w;
-  c##height <- h;
-  Dom.appendChild Dom_html.window##document##body c;
-  c;;
-
 let create_color r g b =
   CSS.Color.string_of_t (CSS.Color.rgb r g b);;
 
@@ -42,12 +25,13 @@ let get_context canvas =
   canvas##getContext (Dom_html._2d_);;
 
 
-let rec loop renderer scene camera mesh cloud_mesh () =
+let rec loop renderer scene camera mesh cloud_mesh stars_mesh () =
   begin
     renderer##render(scene, camera);
-    mesh##rotation##y <- mesh##rotation##y +. (1. /. 64.);
-    cloud_mesh##rotation##y <- cloud_mesh##rotation##y +. (1. /. 63.);
-    Dom_html._requestAnimationFrame (Js.wrap_callback (loop renderer scene camera mesh cloud_mesh));
+    mesh##rotation##y <- mesh##rotation##y +. (1. /. 100.);
+    cloud_mesh##rotation##y <- cloud_mesh##rotation##y +. (1. /. 100.);
+    stars_mesh##rotation##y <- stars_mesh##rotation##y +. (1. /. 15000.);
+    Dom_html._requestAnimationFrame (Js.wrap_callback (loop renderer scene camera mesh cloud_mesh stars_mesh));
   end;;
 
 let create_scene () =
@@ -55,26 +39,27 @@ let create_scene () =
                     [||];;
 
 let create_renderer () =
-  let h = Js.Unsafe.variable "window.innerWidth"
-  and w = Js.Unsafe.variable "window.innerHeight" in
+  let h = Js.Unsafe.variable "screen.width"
+  and w = Js.Unsafe.variable "screen.height" in
   let renderer  = Js.Unsafe.new_obj (Js.Unsafe.variable "THREE.WebGLRenderer")
                                     [||] in
   begin
     renderer##setSize(h, w);
+    renderer##antialias <- Js._true;
     let body = Js.Unsafe.variable "document.body" in
     body##appendChild(renderer##domElement);
     renderer
   end;;
 
 let create_object name params =
-    Js.Unsafe.new_obj (Js.Unsafe.variable name) params;;
+  Js.Unsafe.new_obj (Js.Unsafe.variable name) params;;
 
 let get_variable name =
   Js.Unsafe.variable name;;
 
 let create_camera () =
-  let w = float (Js.Unsafe.variable "window.innerWidth")
-  and h = float (Js.Unsafe.variable "window.innerHeight") in
+  let w = float (Js.Unsafe.variable "screen.width")
+  and h = float (Js.Unsafe.variable "screen.height") in
   let camera =
     Js.Unsafe.new_obj (Js.Unsafe.variable "THREE.PerspectiveCamera")
                       [|Js.Unsafe.inject 0.2;
@@ -91,8 +76,27 @@ let create_sphere r h w  =
   Js.Unsafe.new_obj (Js.Unsafe.variable "THREE.SphereGeometry")
                     [|Js.Unsafe.inject r; Js.Unsafe.inject h; Js.Unsafe.inject w|];;
 
-let create_light color =
-  Js.Unsafe.new_obj (Js.Unsafe.variable "THREE.SpotLight") [|color|];;
+let create_ambiant_light color =
+  create_object "THREE.AmbientLight" [|color|];;
+
+let create_directional_light color direction =
+  let light = create_object "THREE.DirectionalLight" [|color|] in
+  begin
+    light##castShadow <- Js._true;
+    light##shadowCameraNear <- 0.01;
+    light##shadowCameraFar <- 15;
+    light##shadowCameraFov <- 45;
+    light##shadowCameraLeft <- -1;
+    light##shadowCameraRight <- 1;
+    light##shadowCameraTop <- 1;
+    light##shadowCameraBottom <- -1;
+   (* light.shadowCameraVisible <- true; *)
+    light##shadowBias <- 0.001;
+    light##shadowDarkness <- 0.2;
+    light##shadowMapWidth <- 1024;
+    light##shadowMapHeight <- 1024;
+    light
+  end;;
 
 let move_light light x y z =
   light##position##set(x, y, z);;
@@ -114,6 +118,14 @@ let create_cloud_material () =
     material
   end;;
 
+let create_stars_material () =
+  let material = create_object "THREE.MeshBasicMaterial" [||] in
+  begin
+    material##map <- load_texture "textures/galaxy_starfield.png";
+    material##side <- get_variable "THREE.BackSide";
+    material
+  end;;
+
 let create_mesh geometry meterial =
   Js.Unsafe.new_obj (Js.Unsafe.variable "THREE.Mesh")
                     [|Js.Unsafe.inject geometry;Js.Unsafe.inject meterial|];;
@@ -121,34 +133,45 @@ let create_mesh geometry meterial =
 let create_color color =
   create_object "THREE.Color" [|Js.Unsafe.inject color|];;
 
-Dom_html.window##onload <- Dom.handler (fun _ ->
-                                        begin
-                                          debug "ocaml START";
-                                          let renderer = create_renderer () in
-                                          let camera = create_camera () in
-                                          let scene = create_scene () in
-                                          let geometry = create_sphere 0.5 32 32 in
-                                          let material = create_material () in
-                                          let mesh = create_mesh geometry material in
-                                          let light = create_light (Js.Unsafe.inject 0x404040) in
-                                          let cloud_geometry = create_sphere 0.51 32 32 in
-                                          let cloud_material = create_cloud_material () in
-                                          let cloud_mesh = create_mesh cloud_geometry cloud_material in
-                                          mesh##add(cloud_mesh);
-                                          move_light light 10 10 15;
-                                          light##intensity <- 4;
-                                          material##map <- load_texture "textures/earthmap1k.jpg";
-                                          material##bumpMap <- load_texture "textures/earthbump1k.jpg";
-                                          material##bumpScale <- 0.65;
-                                          material##specularMap <- load_texture "textures/earthspec1k.jpg";
-                                          material##specular <- create_color "gray";
-                                          renderer##alpha <- Js._true;
-                                          renderer##setClearColor(0xffffff, 1);
-                                          scene##add(mesh);
-                                          scene##add(light);
-                                          debug "ocaml END";
-                                          Dom_html._requestAnimationFrame
-                                            (Js.wrap_callback
-                                               (loop renderer scene camera mesh cloud_mesh));
-                                          Js._true
-                                        end);;
+  Dom_html.window##onload <- Dom.handler (fun _ ->
+                                          begin
+                                            debug "ocaml START";
+                                            let renderer = create_renderer () in
+                                            let camera = create_camera () in
+                                            let scene = create_scene () in
+                                            let earth_geometry = create_sphere 0.5 32 32 in
+                                            let earth_material = create_material () in
+                                            let earth_mesh = create_mesh earth_geometry earth_material in
+                                            let cloud_geometry = create_sphere 0.51 32 32 in
+                                            let cloud_material = create_cloud_material () in
+                                            let cloud_mesh = create_mesh cloud_geometry cloud_material in
+                                            let stars_geometry = create_sphere 2 32 32 in
+                                            let stars_material = create_stars_material () in
+                                            let stars_mesh = create_mesh stars_geometry stars_material in
+                                            let ambiant_light = create_ambiant_light
+                                                                  (Js.Unsafe.inject 0x222222) in
+                                            let directional_light = create_directional_light
+                                                                      (Js.Unsafe.inject 0xffffff)
+                                                                      (Js.Unsafe.inject 1) in
+                                            move_light directional_light 5 5 5;
+                                            earth_mesh##add(cloud_mesh);
+                                            earth_material##map <- load_texture
+                                                                     "textures/earthmap1k.jpg";
+                                            earth_material##bumpMap <- load_texture
+                                                                         "textures/earthbump1k.jpg";
+                                            earth_material##bumpScale <- 0.65;
+                                            earth_material##specularMap <- load_texture
+                                                                             "textures/earthspec1k.jpg";
+                                            earth_material##specular <- create_color "gray";
+                                            renderer##alpha <- Js._true;
+                                            renderer##setClearColor(0xffffff, 1);
+                                            scene##add(earth_mesh);
+                                            scene##add(stars_mesh);
+                                            scene##add(ambiant_light);
+                                            scene##add(directional_light);
+                                            debug "ocaml END";
+                                            Dom_html._requestAnimationFrame
+                                              (Js.wrap_callback
+                                                 (loop renderer scene camera earth_mesh cloud_mesh stars_mesh));
+                                            Js._true
+                                          end);;
